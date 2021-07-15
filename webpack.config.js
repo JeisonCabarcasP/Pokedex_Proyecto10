3febee4ed03e2e5be87cba7c92842d76a73517a3
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-15 15:39:44 -0500

    Configuracion de enterno de trabajo

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: "./src/App.js",
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+
+                    {
+                        loader: "file-loader"
+                    }
+                ],
+            },
+
+        ],
+
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ],
+
+}
