28738d5c1f33210dd15dbcd4a41f072bd8cc4d8a
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-15 23:03:37 -0500

    modificacion del header

--- src/Componets/Header/Header.js
+++ src/Componets/Header/Header.js
@@ -3,7 +3,6 @@ import logo from "../../assets/imgs/logo.png"
 const Header = () => {
   const view = `
 <div class="header-container">
-  <img src="${logo}" alt="Logo">
   <a href="#">Pokedex</a>
 </div>
 `;
